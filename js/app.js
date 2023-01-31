e8f660cb2327b7d718f3d988c33d672364ca6c9b
xxentasion <107941903+xxentasion@users.noreply.github.com>
2023-02-01 00:16:00 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+new Swiper('.slider', {
+    speed: 2400,
+    mousewheel: {
+        enabled: true,
+        sensitivity: 7,
+    } ,
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true,
+})
